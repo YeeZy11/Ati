7b6a42d921574b863da67d37195af672bd8b2209
Pedro Dutra <pedro_dutra@estudante.sc.senai.br>
2019-06-23 01:28:49 -0300

    Criando funcao para renderizar todos

--- Mod3/todos.js
+++ Mod3/todos.js
@@ -3,3 +3,25 @@ var inputElement = document.querySelector("#app input")
 var buttonElement = document.querySelector("#app button")
 
 
+function renderTodos() {
+    listElement.innerHTML = "";
+    for (todo of todos){
+        var todoElement = document.createElement("li");
+        
+        var todoText = document.createTextNode(todo);
+
+        var linkElement = document.createElement("a");
+        linkElement.setAttribute("href", "#");
+
+        var pos = todos.indexOf(todo);
+        linkElement.setAttribute("onclick", "DeleteTodo("+pos+")")
+
+        var linkText = document.createTextNode("Excluir");
+        linkElement.appendChild(linkText);
+
+
+        todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
+        listElement.appendChild(todoElement);
+    }
+}
