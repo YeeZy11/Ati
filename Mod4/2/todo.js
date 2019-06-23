e629187e9f7ee20266d6153cc842b75b5f38bdf4
Pedro Dutra <pedro_dutra@estudante.sc.senai.br>
2019-06-23 02:03:02 -0300

    Criando a funcao para renderizar o repositorio

--- Mod4/2/todo.js
+++ Mod4/2/todo.js
@@ -1,2 +1,11 @@
 var listElement = document.querySelector('ul');
 var inputElement = document.querySelector('input');
+
+function renderRepositorios(repositorios) {
+    for (repo of repositorios) {
+      const textElement = document.createTextNode(repo.name);
+      const liElement = document.createElement('li');
+      liElement.appendChild(textElement);
+      listElement.appendChild(liElement);
+    }
+  }
